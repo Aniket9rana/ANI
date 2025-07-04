05555867fada2ff63b1060180cb5f7bc614bbc03
Aniket9rana <aniketrana5892@gmail.com>
2025-07-04 15:50:33 +0530

    cd

---
+++ package/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,
+
+setup(
+    name='ANI-STT',version='1.0.0',
+    author='Aniket Rana',
+    author_email="aniketrana5892@gmail.com",
+    description='A simple Speech to Text converter using Selenium and Webdriver',
+)
+packages = find_packages(),
+install_requirements = [
+    'selenium',
+    'webdriver-manager',
+]
